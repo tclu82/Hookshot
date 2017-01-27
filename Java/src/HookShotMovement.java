6ab2366be7e2a2a837f01dfba087bfa7a951d546
Zac <tclu82@uw.edu>
2017-01-27 12:27:18 -0800

    update swing movement with js code

--- Java/src/HookShotMovement.java
+++ Java/src/HookShotMovement.java
@@ -72,7 +72,10 @@ public class HookShotMovement {
         for (int degree=0; degree<=180; degree+= moveByDegree) {
 
             /**
-             * In Java, Math.cos(Math.toRadians(degree)) can be writte Math.sin(30 / 180 * Math.PI) in JavaScript or
+             * In JavaScript:
+             *
+             * currentPoint.x = -(Math.cos(degree / 180 * Math.PI) - 1) * hookShotLength;
+             * currentPoint.y = Math.sin(degree / 180 * Math.PI) * hookShotLength;
 
                  // Converts from degrees to radians.
                  Math.radians = function(degrees) {
@@ -83,13 +86,13 @@ public class HookShotMovement {
                  y = Math.sin(Math.radians(theDegreeWeWantT));
              *
              */
-            currentPoint.x = (Math.cos(Math.toRadians(degree)) - 1) * hookShotLength;
+            currentPoint.x = -(Math.cos(Math.toRadians(degree)) - 1) * hookShotLength;
 
             currentPoint.y = Math.sin(Math.toRadians(degree)) * hookShotLength;
 
             String position = String.format("Degree: %d, X: %.3f, Y: %.3f",
                     degree,
-                    -currentPoint.x,
+                    currentPoint.x,
                     currentPoint.y);
 
             System.out.println(position);
@@ -106,7 +109,7 @@ public class HookShotMovement {
         // Decide how long hero's hook shot is
         HookShotMovement hsm = new HookShotMovement(100);
         // Change the integer degree 0~180, the smaller degree number, display more positions
-        hsm.heroMove(45);
+        hsm.heroMove(10);
     }
 
     /**
